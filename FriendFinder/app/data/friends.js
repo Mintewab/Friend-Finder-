2752f4731439794283e94f8dc9f8bece71ad7ac2
Mintewab <demissiewminte123@gmail.com>
2019-06-25 23:10:28 -0700

    created my  friend which is another American WOmen Soccer player

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -3,5 +3,9 @@ var friendMatch = [
       name: "Alex Morgan",
       photo: "https://image.oregonlive.com/home/olive-media/width600/img/portland-thorns/photo/alex-morgancarla-guerrero-db7ef7e31c5e4880.jpg ",
       scores: ["3", "3", "3", "3", "1"]
-    }
+    }, {
+        name: "Allie Long",
+        photo: "https://girlssoccernetwork.com/wp-content/uploads/2017/10/Allie-Long.png ",
+        scores: ["1", "1", "1", "1", "1"]
+      }, 
 ];
