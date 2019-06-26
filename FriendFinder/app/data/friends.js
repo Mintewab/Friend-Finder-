30c403d05cc15f5c67aa82d77834d6c10b517282
Mintewab <demissiewminte123@gmail.com>
2019-06-25 23:09:31 -0700

    created my first friend which is my favorite Soccer player

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,1 +1,7 @@
-var friendMatch = 
+var friendMatch = [
+    {
+      name: "Alex Morgan",
+      photo: "https://image.oregonlive.com/home/olive-media/width600/img/portland-thorns/photo/alex-morgancarla-guerrero-db7ef7e31c5e4880.jpg ",
+      scores: ["3", "3", "3", "3", "1"]
+    }
+];
