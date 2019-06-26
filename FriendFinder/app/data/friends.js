006bc0a4d177ebbdc754e6622686a61787c96aa6
Mintewab <demissiewminte123@gmail.com>
2019-06-25 23:12:11 -0700

    created another friend which is another American WOmen Soccer player

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,9 +1,10 @@
 var friendMatch = [
     {
-      name: "Alex Morgan",
-      photo: "https://image.oregonlive.com/home/olive-media/width600/img/portland-thorns/photo/alex-morgancarla-guerrero-db7ef7e31c5e4880.jpg ",
-      scores: ["3", "3", "3", "3", "1"]
-    }, {
+        name: "Alex Morgan",
+        photo: "https://image.oregonlive.com/home/olive-media/width600/img/portland-thorns/photo/alex-morgancarla-guerrero-db7ef7e31c5e4880.jpg ",
+        scores: ["3", "3", "3", "3", "1"]
+        
+      }, {
         name: "Allie Long",
         photo: "https://girlssoccernetwork.com/wp-content/uploads/2017/10/Allie-Long.png ",
         scores: ["1", "1", "1", "1", "1"]
@@ -12,5 +13,9 @@ var friendMatch = [
         photo: "https://sportshub.cbsistatic.com/i/2019/06/24/61b6b720-4e90-4e05-b61b-e3285c2553be/usatsi-12949052-megan-rapinoe-1st-pk-goal-uswnt-2019-ww-c-r16-1400.jpg ",
         scores: ["3", "3", "3", "3", "3"]
        
-      }, 
+      }, {
+        name: "Crystal Dunn",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm2E1PrS0BPl1FFktJ4TpRD4yai6SzVKBf_0IC4otJeI8Er2W_ ",
+        scores: ["2", "2", "2", "2", "2"]
+      },
 ];
