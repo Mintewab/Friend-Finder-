a9113c328de3a0e9a8d823a81caf2460eb947c7f
Mintewab <demissiewminte123@gmail.com>
2019-06-27 12:30:36 -0700

    created app.get

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,3 +1,10 @@
 var friendMatch = require("../data/friends.js");
 
-module.exports = function(app) 
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+      res.json(friendMatch);
+    });
+
+
+
+};
