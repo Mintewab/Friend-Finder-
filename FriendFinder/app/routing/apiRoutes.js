c03e4611cfb674ba5fe32ae8dc016ccec94befd3
Mintewab <demissiewminte123@gmail.com>
2019-06-27 12:31:08 -0700

    created app.post

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -4,7 +4,18 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
       res.json(friendMatch);
     });
-
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+    
+        for (var i = 0; i < newFriend.scores.length; i++) {
+          if (newFriend.scores[i] == "1 (Yes)") {
+            newFriend.scores[i] = 1;
+          } else if (newFriend.scores[i] == "3 (No)") {
+            newFriend.scores[i] = 3;
+          } else {
+            newFriend.scores[i] = parseInt(newFriend.scores[i]);
+          }
+        }
 
 
 };
